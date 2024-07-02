567e5e323eb92e4db8f7c02b92a9700415615168
yiqun Liang <herman@koii.network>
2024-07-02 10:19:10 -0300

    Refactor DB

--- api/saveTweetsToMongoDB.js
+++ api/saveTweetsToMongoDB.js
@@ -3,6 +3,8 @@ const axios = require("axios");
 require("dotenv").config();
 
 const DB_KEY = process.env.DB_KEY;
+const DB_NAME = process.env.DB_NAME;
+const COLLECTION_NAME = process.env.COLLECTION_NAME;
 
 async function saveTweetsToMongoDB(tweetList) {
  const client = new MongoClient(DB_KEY);
@@ -12,8 +14,8 @@ async function saveTweetsToMongoDB(tweetList) {
     console.log("Connected to MongoDB.");
 
     // Select the collection and database by name from environment variables
-    const db = client.db(process.env.DB_NAME);
-    const collection = db.collection(process.env.COLLECTION_NAME);
+    const db = client.db(DB_NAME);
+    const collection = db.collection(COLLECTION_NAME);
 
     // Create a unique index on the 'tweets_id' field to prevent duplicate entries
     await collection.createIndex({ "tweets_id": 1 }, { unique: true });
