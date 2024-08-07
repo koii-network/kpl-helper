d9fd6f984d1d98141f9bfe023be1463e538a991e
Soma <soma@koii.network>
2024-08-07 16:49:19 -0300

    update keywords fetch

--- server.js
+++ server.js
@@ -1,78 +1,81 @@
-const express = require('express');
-const axios = require('axios');
-const getTaskData = require('./helpers/getTaskData'); 
-require('dotenv').config();
+const express = require("express");
+const axios = require("axios");
+const getTaskData = require("./helpers/getTaskData");
+require("dotenv").config();
 const app = express();
 const port = 3000;
 
-app.use(express.json()); 
+app.use(express.json());
 
 // Custom keywords or randomly selected
 const defaultKeyword = process.env.KEY_WORD;
 let pendingRequests = [];
 
-app.get('/keywords', (req, res) => {
+app.get("/keywords", (req, res) => {
   try {
-    const { stakingKey } = req.query;
-
+    console.log("Request received:", req.query);
+    const stakingKey = req.query;
+    console.log("Staking Key:", stakingKey.key);
     if (!stakingKey) {
-      return res.status(400).send('stakingKey is required');
+      return res.status(400).send("stakingKey is required");
     }
 
     let keyword;
 
     if (pendingRequests.length > 0) {
       keyword = pendingRequests.shift(); // Get keyword from pendingRequests and remove it
-      console.log('Keyword from pendingRequests:', keyword);
+      console.log("Keyword from pendingRequests:", keyword);
     } else {
       keyword = defaultKeyword;
       if (!keyword) {
-        console.log('No Keywords from middle server, loading local keywords.json');
-        const wordsList = require('./keywords.json');
+        console.log(
+          "No Keywords from middle server, loading local keywords.json"
+        );
+        const wordsList = require("./keywords.json");
         const randomIndex = Math.floor(Math.random() * wordsList.length);
         keyword = wordsList[randomIndex]; // Load local JSON data
-        console.log('Keyword from json:', keyword);
+        console.log("Keyword from json:", keyword);
       }
     }
-    
-    res.send(keyword);
 
+    res.send(keyword);
   } catch (error) {
-    console.log('Error processing request:', error);
-    res.status(500).send('Internal Server Error');
+    console.log("Error processing request:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
 // Endpoint to add pending keywords
-app.post('/add-pending-keywords', (req, res) => {
+app.post("/add-pending-keywords", (req, res) => {
   try {
     const { keywords } = req.body;
 
     if (!keywords || !Array.isArray(keywords)) {
-      return res.status(400).send('Invalid input, array of keywords is required');
+      return res
+        .status(400)
+        .send("Invalid input, array of keywords is required");
     }
 
     pendingRequests = pendingRequests.concat(keywords);
-    console.log('Pending requests updated:', pendingRequests);
-    
-    res.send('Keywords added to pending requests');
+    console.log("Pending requests updated:", pendingRequests);
 
+    res.send("Keywords added to pending requests");
   } catch (error) {
-    console.log('Error processing request:', error);
-    res.status(500).send('Internal Server Error');
+    console.log("Error processing request:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
 // Get taskstate from running task
-app.get('/taskstate', async (req, res) => {
+app.get("/taskstate", async (req, res) => {
   try {
     const taskID = process.env.TASK_ID;
     const round = 0;
     const taskData = await getTaskData(taskID, round);
     res.json({ taskData });
   } catch (error) {
-    console.error('Error fetching task data:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching task data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
