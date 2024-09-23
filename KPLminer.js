4aac7a556d17f8e1fd7bf430c66a3b1edce119df
Soma <soma@koii.network>
2024-09-23 14:28:01 -0300

    fix token miner

--- KPLminer.js
+++ KPLminer.js
@@ -1,8 +1,38 @@
 const getTaskData = require("./helpers/getTaskData");
+const transferKPL = require("./helpers/transferKPL");
 
 async function main() {
-    const taskData = await getTaskData('DLfP5RCAdYiirmn8cigptQusg8ds75JzFXSrHc8ydyeu', 0);
-    console.log(taskData);
+  const listOfKPLs = [
+    "us8mD4jGkFWnUuAHB8V5ZcrAosEqi2h1Zcyca68QA1G", // BIRD
+    "9UcQaSsBTeXowhMBgSbTEeQubGHxXDNJRjz4s7uxibTP", // BB
+    "FJG2aEPtertCXoedgteCCMmgngSZo1Zd715oNBzR7xpR", // FIRE
+    "BmdvRw51zhCKfkBmw6jmLawJafimTMtZ2eVwT3fL2WM6", // RATs
+  ];
+  const taskData = await getTaskData(
+    "DLfP5RCAdYiirmn8cigptQusg8ds75JzFXSrHc8ydyeu",
+    0
+  );
+//   console.log(taskData);
+
+  const randomKPL = listOfKPLs[Math.floor(Math.random() * listOfKPLs.length)];
+
+  // Split 500 KPL among all submission addresses
+  const totalKPL = 500;
+  const addresses = taskData.submissions;
+  let kplPerSubmission = totalKPL / addresses.length;
+
+  kplPerSubmission = parseFloat(kplPerSubmission.toFixed(2)); 
+
+  console.log("Random KPL: ", randomKPL);
+  console.log("Total KPL: ", totalKPL);
+  console.log("KPL per submission: ", kplPerSubmission);
+  console.log("Addresses: ", addresses.length);
+
+//   for (let i = 0; i < addresses.length; i++) {
+//     const address = addresses[i];
+//     submission.kpl = kplPerSubmission;
+//     await transferKPL(randomKPL, address, kplPerSubmission);
+//   }
 }
 
 main();
