4572ae532e9d682e76484cbc3c81c69e4e954ae6
Laura Abro <laura@koii.network>
2025-01-16 15:25:45 -0400

    make client global, don't recursively call main

--- KPLminer.js
+++ KPLminer.js
@@ -7,14 +7,13 @@ const uri = process.env.DB_KEY;
 const DB_name = "mainnet_airdrop_middleman";
 const collection_name = "kplminer";
 
-async function hasRoundTransferred(round) {
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+const client = new MongoClient(uri);
+client.connect().then(() => {
+  main();
+});
 
+async function hasRoundTransferred(round) {
   try {
-    await client.connect();
     const database = client.db(DB_name);
     const transfersCollection = database.collection(collection_name);
 
@@ -24,19 +23,11 @@ async function hasRoundTransferred(round) {
   } catch (error) {
     console.error("Error checking round transfer:", error);
     return false; // Return false if error occurs
-  } finally {
-    await client.close(); // Ensure the connection is closed
   }
 }
 
 async function recordTransfer(round, kplToken, transfers) {
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
   try {
-    await client.connect();
     const database = client.db(DB_name);
     const transfersCollection = database.collection(collection_name);
 
@@ -51,80 +42,82 @@ async function recordTransfer(round, kplToken, transfers) {
     console.log(`Recorded transfer for round ${round}`);
   } catch (error) {
     console.error("Error recording transfer:", error);
-  } finally {
-    await client.close(); // Ensure the connection is closed
   }
+}
+
+async function processRound() {
+  // mainnet kpl token mint addresses
+  const listOfKPLs = [
+    "Fe7878UvoGHUM7B8C95rX3MigUqtBZmtcPcX5sz3Qhxd", // BIRD
+    "AayQB5XgchRR9zTr5bi95aQ1FFtJ6jEVmbrBQRx5m4gw", // BB
+    "CJLjkLohs3PScpBQ3r1WKHjQsWYPq291MgPjpTWTXNc4", // ASTRO
+    "4qayyw53kWz6GzypcejjT1cvwMXS1qYLSMQRE8se3gTv", // FIRE
+    "9VUq3SfNDh1bgwamF8tr9aGvuXvNi6ktjx1Edc2da7ey", // VIP
+    // "5WWwTwzaM9So2pxAY3wzktTDHARhhnRmSvYpxxw5wLkJ", // FISH
+    // "CgdFgTz2iX9B63XASmraYExu9gTs27DfK7JW4S6tbmgD", // KVN
+    // "n3Rep7GRh3jgkGXaULNu8WzfuwCC9Rcrv82mxzJMnnH", // SONO
+    // "BmdvRw51zhCKfkBmw6jmLawJafimTMtZ2eVwT3fL2WM6", // RATs
+    // "NGFruaQX9xHqWv195RNQL2wtq2LJwTmnkE9XjGAZKHx", // SOMA
+    // "EErjDSPHmjz9ZipEtVoN54QzCjPvePBADvXcWKT659NW", // SSS
+    // "HK8STMDTos4QFocyEADxwdXBrN13DCB9AcwLXsmvBthh" // SMART
+  ];
+
+  let addresses = [];
+
+  const taskData = await getTaskData(
+    "8CQT6QJN8ZUJWHgBAJBKxbuyBuE43fxA1jMGBGQPbCUZ",
+    0
+  );
+
+  const randomKPL = listOfKPLs[Math.floor(Math.random() * listOfKPLs.length)];
+
+  // Split 500 KPL among all submission addresses
+  // const totalKPL = 3000;
+  addresses = taskData.submissions;
+  // let kplPerSubmission = totalKPL / addresses.length;
+  // kplPerSubmission = parseFloat(kplPerSubmission.toFixed(2));
+
+  let kplPerSubmission = 0.2;
+
+  console.log("Miner round: ", taskData.maxRound);
+  console.log("Random KPL: ", randomKPL);
+  console.log("KPL per submission: ", kplPerSubmission);
+  console.log("Addresses: ", addresses.length);
+
+  let checkTransferred = await hasRoundTransferred(taskData.maxRound);
+  if (checkTransferred) {
+    console.log("Round already transferred", taskData.maxRound);
+  } else {
+    // Transfer KPL to all addresses
+    for (let i = 0; i < addresses.length; i++) {
+      const address = addresses[i];
+      const transferResult = await transferKPL(
+        randomKPL,
+        address,
+        kplPerSubmission
+      );
+
+      if (transferResult) {
+        console.log(`Transferred ${kplPerSubmission} KPL to ${address}`);
+      }
+    }
+    // Record the transfer after processing all addresses
+    await recordTransfer(taskData.maxRound, randomKPL, addresses);
+  }
+
+  // Wait for the next round
+  console.log("Waiting for the next round...");
 }
 
 async function main() {
-  try {
-    // mainnet kpl token mint addresses
-    const listOfKPLs = [
-      "Fe7878UvoGHUM7B8C95rX3MigUqtBZmtcPcX5sz3Qhxd", // BIRD
-      "AayQB5XgchRR9zTr5bi95aQ1FFtJ6jEVmbrBQRx5m4gw", // BB
-      "CJLjkLohs3PScpBQ3r1WKHjQsWYPq291MgPjpTWTXNc4", // ASTRO
-      "4qayyw53kWz6GzypcejjT1cvwMXS1qYLSMQRE8se3gTv", // FIRE
-      "9VUq3SfNDh1bgwamF8tr9aGvuXvNi6ktjx1Edc2da7ey", // VIP
-      // "5WWwTwzaM9So2pxAY3wzktTDHARhhnRmSvYpxxw5wLkJ", // FISH
-      // "CgdFgTz2iX9B63XASmraYExu9gTs27DfK7JW4S6tbmgD", // KVN
-      // "n3Rep7GRh3jgkGXaULNu8WzfuwCC9Rcrv82mxzJMnnH", // SONO
-      // "BmdvRw51zhCKfkBmw6jmLawJafimTMtZ2eVwT3fL2WM6", // RATs
-      // "NGFruaQX9xHqWv195RNQL2wtq2LJwTmnkE9XjGAZKHx", // SOMA
-      // "EErjDSPHmjz9ZipEtVoN54QzCjPvePBADvXcWKT659NW", // SSS
-      // "HK8STMDTos4QFocyEADxwdXBrN13DCB9AcwLXsmvBthh" // SMART
-
-    ];
-
-    let addresses = [];
-
-    const taskData = await getTaskData(
-      "8CQT6QJN8ZUJWHgBAJBKxbuyBuE43fxA1jMGBGQPbCUZ",
-      0
-    );
-    
-    const randomKPL = listOfKPLs[Math.floor(Math.random() * listOfKPLs.length)];
-
-    // Split 500 KPL among all submission addresses
-    // const totalKPL = 3000;
-    addresses = taskData.submissions;
-    // let kplPerSubmission = totalKPL / addresses.length;
-    // kplPerSubmission = parseFloat(kplPerSubmission.toFixed(2));
-
-    let kplPerSubmission = 0.2;
-
-    console.log("Miner round: ", taskData.maxRound)
-    console.log("Random KPL: ", randomKPL);
-    console.log("KPL per submission: ", kplPerSubmission);
-    console.log("Addresses: ", addresses.length);
-
-    let checkTransferred = await hasRoundTransferred(taskData.maxRound);
-    if (checkTransferred) {
-      console.log("Round already transferred", taskData.maxRound);
-    } else {
-      // Transfer KPL to all addresses
-      for (let i = 0; i < addresses.length; i++) {
-        const address = addresses[i];
-        const transferResult = await transferKPL(randomKPL, address, kplPerSubmission);
-        
-        if (transferResult) {
-          console.log(`Transferred ${kplPerSubmission} KPL to ${address}`);
-        }
-      }
-      
-      // Record the transfer after processing all addresses
-      await recordTransfer(taskData.maxRound, randomKPL, addresses);
+  while (true) {
+    try {
+      await processRound();
+    } catch (error) {
+      console.error("Error in main function:", error);
+      console.log("Retrying in 5 seconds");
+    } finally {
+      await new Promise((resolve) => setTimeout(resolve, 5000));
     }
-
-    // Wait for the next round
-    console.log("Waiting for the next round...");
-    await new Promise((resolve) => setTimeout(resolve, 600000));
-    main();
-  } catch (error) {
-    console.error("Error in main function:", error);
-    console.log("Retrying in 5 seconds");
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-    main();
   }
 }
-
-main();
